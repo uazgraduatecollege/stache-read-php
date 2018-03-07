8728bd896d96e415cf8bac4bf73258253a7fa11e
Patrick Barabe <pbarabe@email.arizona.edu>
2018-03-07 11:00:00 -0700

    Support PHP 5 & 7 compatibility
    Removing composer.lock so that installation can be compatible with both
    PHP 5 and 7.
    
    Also updating tests & CI test execution so that PHP 5 can be tested
    when using an older version of PHPUnit compatible w/PHP5

---
+++ tests/php5/Uagc/StacheReaderTest.php
@@ -0,0 +1,80 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class StacheReaderTest extends TestCase
+{
+
+    public function testCanBeInstantiatedWithValidParams()
+    {
+        $this->assertInstanceOf(
+            Uagc\StacheReader::class,
+            new Uagc\StacheReader([
+                'protocol' => 'http',
+                'port' => '123',
+                'path' => '/one/two/three/',
+                'domain' => 'one.two.com',
+                'user_agent' => 'Test Agent'
+            ])
+        );
+    }
+
+    public function testCannotBeInstantiatedWithNoParams()
+    {
+        // at miminum, 'domain' must be passed
+        $this->setExpectedException(InvalidArgumentException::class);
+        new Uagc\StacheReader();
+    }
+
+    public function testCannotBeInstantiatedWithInvalidParams()
+    {
+        // protocol must be 'http' or 'https'
+        $this->setExpectedException(InvalidArgumentException::class);
+        new Uagc\StacheReader(['protocol' => 'invalid']);
+
+        // port must be a number
+        $this->setExpectedException(InvalidArgumentException::class);
+        new Uagc\StacheReader(['port' => 'invalid']);
+
+        // path must be a string (or empty value)
+        $this->setExpectedException(InvalidArgumentException::class);
+        new Uagc\StacheReader(['path' => ['invalid' => 'invalid']]);
+
+        // domain must be a string (or empty value)
+        $this->setExpectedException(InvalidArgumentException::class);
+        new Uagc\StacheReader(['domain' => ['invalid' => 'invalid']]);
+
+        // user_agent must be a string (or empty value)
+        $this->setExpectedException(InvalidArgumentException::class);
+        new Uagc\StacheReader(['user_agent' => ['invalid' => 'invalid']]);
+
+    }
+
+    public function testReadFailsWithoutValidArgs()
+    {
+        $this->setExpectedException(InvalidArgumentException::class);
+        $sr = new Uagc\StacheReader(['domain' => 'one.two.com']);
+        $sr->read();
+    }
+
+    public function testReadWorksWithValidArgs()
+    {
+        // this test only runs if these env vars are set
+        if (getenv('STACHE_TEST_DOMAIN') &&
+            getenv('STACHE_TEST_ITEM') &&
+            getenv('STACHE_TEST_ITEMKEY')) {
+
+            $sr = new Uagc\StacheReader(['domain' => getenv('STACHE_TEST_DOMAIN')]);
+            $sItem = $sr->read(
+                getenv('STACHE_TEST_ITEM'),
+                getenv('STACHE_TEST_ITEMKEY')
+            );
+
+            $this->assertTrue(
+                is_object($sItem)
+            );
+        }
+    }
+
+}
+
